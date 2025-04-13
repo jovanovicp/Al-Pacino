eea1943593f4b6afd58e522a454777ad5837f25f
Petar Jovanovic <85284177+jovanovicp@users.noreply.github.com>
2025-04-13 12:30:59 +0200

    Delete Al Pacino directory

--- Al Pacino/js/map.js
+++
@@ -1,6 +0,0 @@
-function initMap() {
-  var zemun = {lat: 44.848821, lng: 20.410778};
-  var mapa = new google.maps.Map(
-      document.getElementById('mapa'), {zoom: 18, center: zemun});
-  var marker = new google.maps.Marker({position: zemun, map: mapa});
-}
