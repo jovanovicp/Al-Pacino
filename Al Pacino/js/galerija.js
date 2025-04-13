903a2962279ee69a16ffc7d6080ca8dba1751c91
Petar Jovanovic <85284177+jovanovicp@users.noreply.github.com>
2025-04-13 12:21:16 +0200

    Add files via upload

---
+++ Al Pacino/js/galerija.js
@@ -0,0 +1,54 @@
+$(".slika1").hover(function(){
+  $(this).css(
+  {"width": "300px", "height": "300px", "background-image": "url('img/galerija1veca.jpg')", "margin-right": "0px"}
+  );
+  }, function(){
+  $(this).css(
+  {"width": "270px", "height": "270px", "background-image": "url('img/galerija1.jpg')", "margin-right": "30px"}
+  );
+});
+$(".druga").hover(function(){
+  $(this).css(
+  {"width": "300px", "height": "300px", "background-image": "url('img/galerija2veca.jpg')", "margin-right": "10px", "margin-left": "-20px"}
+  );
+  }, function(){
+  $(this).css(
+  {"width": "270px", "height": "270px", "background-image": "url('img/galerija2.jpg')", "margin-right": "30px", "margin-left": "0px"}
+  );
+});
+$(".treca").hover(function(){
+  $(this).css(
+  {"width": "300px", "height": "300px", "background-image": "url('img/galerija3veca.jpg')", "margin-right": "0px", "margin-left": "-30px"}
+  );
+  }, function(){
+  $(this).css(
+  {"width": "270px", "height": "270px", "background-image": "url('img/galerija3.jpg')", "margin-right": "0px", "margin-left": "0px"}
+  );
+});
+$(".cetvrta").hover(function(){
+  $(this).css(
+  {"width": "300px", "height": "300px", "background-image": "url('img/galerija4veca.jpg')", "margin-right": "0px"}
+  );
+  }, function(){
+  $(this).css(
+  {"width": "270px", "height": "270px", "background-image": "url('img/galerija4.jpg')", "margin-right": "30px"}
+  );
+});
+$(".peta").hover(function(){
+  $(this).css(
+  {"width": "300px", "height": "300px", "background-image": "url('img/galerija5veca.jpg')", "margin-right": "10px", "margin-left": "-20px"}
+  );
+  }, function(){
+  $(this).css(
+  {"width": "270px", "height": "270px", "background-image": "url('img/galerija5.jpg')", "margin-right": "30px", "margin-left": "0px"}
+  );
+});
+$(".sesta").hover(function(){
+  $(this).css(
+  {"width": "300px", "height": "300px", "background-image": "url('img/galerija6veca.jpg')", "margin-right": "0px", "margin-left": "-30px"}
+  );
+  }, function(){
+  $(this).css(
+  {"width": "270px", "height": "270px", "background-image": "url('img/galerija6.jpg')", "margin-right": "0px", "margin-left": "0px"}
+  );
+});
